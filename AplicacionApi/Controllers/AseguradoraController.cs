260b4f7ac420bf24d31e34e74423c1bd700ab1f4
Elias <afloresp3@unemi.edu.ec>
2023-10-17 14:42:19 -0500

    Agregacion de controller

---
+++ AplicacionApi/Controllers/AseguradoraController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using AplicacionApi.Data;
+
+namespace AplicacionApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AseguradoraController : ControllerBase
+    {
+        private AseguradoraContext _context;
+
+        public AseguradoraController(AseguradoraContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Seguros);
+        }
+
+    }
+}
